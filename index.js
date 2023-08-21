6d40038e98e96473e940a70dd9ee306aac2b9e86
Leonardo Moretti <leonardonabarro2003@gmail.com>
2023-08-21 16:48:48 -0300

    arrumando a validação do formulario para não ter números repetidos

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ function addRow() {
     const inputNameContact = document.getElementById('name-contact')
     const inputNumberContact = document.getElementById('number-contact')
 
+    if (numbers.includes(inputNumberContact.value)) {
+        alert(`O número: ${inputNumberContact.value} já foi adicionado a sua lista de contatos.`)
+    } else {
+        numbers.push(inputNumberContact.value)
 
-    let row = '<tr>'
-    row += `<td>${inputNameContact.value}</td>`
-    row += `<td>${inputNumberContact.value}</td>`
-    row += '</tr>'
-    rows += row
+        let row = '<tr>'
+        row += `<td>${inputNameContact.value}</td>`
+        row += `<td>${inputNumberContact.value}</td>`
+        row += '</tr>'
+        rows += row
+    }
+
 
     inputNameContact.value = ''
     inputNumberContact.value = ''
